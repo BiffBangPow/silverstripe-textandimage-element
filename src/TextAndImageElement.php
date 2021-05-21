98d4f77c6e8474766c947865088a193700b63534
Tim Burt <tim@biffbangpow.com>
2021-05-21 11:05:48 +0100

    Base code

---
+++ src/TextAndImageElement.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace BiffBangPow\Element;
+
+use BiffBangPow\Extension\TextPositionExtension;
+use DNADesign\Elemental\Models\BaseElement;
+use Sheadawson\Linkable\Forms\LinkField;
+use Sheadawson\Linkable\Models\Link;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\FieldList;
+
+class TextAndImageElement extends BaseElement
+{
+    /**
+     * @var string
+     */
+    private static $table_name = 'ElementTextAndImage';
+
+    private static $singular_name = 'text and image element';
+
+    private static $plural_name = 'text and image elements';
+
+    private static $description = 'Displays text with an image on either the left or right';
+
+    /**
+     * @var array
+     */
+    private static $db = [
+        'Text' => 'HTMLText',
+    ];
+
+
+    /**
+     * @var array
+     */
+    private static $has_one = [
+        'CallToAction' => Link::class,
+        'Image' => Image::class,
+    ];
+
+    /**
+     * @var array
+     */
+    private static $owns = [
+        'CallToAction',
+        'Image',
+    ];
+
+    /**
+     * @var array
+     */
+    private static $extensions = [
+        TextPositionExtension::class,
+    ];
+
+    /**
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $fields->addFieldsToTab(
+            'Root.Main',
+            [
+                LinkField::create(
+                    'CallToActionID',
+                    'Call To Action'
+                ),
+            ]
+        );
+
+        return $fields;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return 'Text and Image';
+    }
+
+    public function getSimpleClassName()
+    {
+        return 'text-and-image-element';
+    }
+}
---
+++ templates/BiffBangPow/Element/TextAndimageElement.ss
@@ -0,0 +1,18 @@
+<div class="py-4 py-xl-7">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6 d-flex align-items-center py-4 <% if $TextPosition == 'Right' %>order-lg-2<% end_if %>">
+                <div class="text">
+                    <% if $ShowTitle %>
+                        <h3 class="mb-4 title">$MarkdownText.Title.RAW</h3>
+                    <% end_if %>
+                    <div>$Text</div>
+                    <% include Link %>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 d-flex align-items-center <% if $TextPosition == 'Right' %> order-lg-1<% end_if %>">
+                <img class="image w-100" title="$Image.Title" alt="$Image.Title" src="$Image.ScaleMaxWidth(960).Link" />
+            </div>
+        </div>
+    </div>
+</div>
